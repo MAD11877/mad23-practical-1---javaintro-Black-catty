be2b1a2d1c9b11b8268f8bf05993c03d7a5aee0e
Black-catty <105922354+Black-catty@users.noreply.github.com>
2023-04-19 00:32:37 +0800

    Question 4

--- Question4.java
+++ Question4.java
@@ -18,6 +18,21 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
+    int base= in.nextInt();      
+    //inner loop  
+    for (int i= base-1; i>=0 ; i--)  
+    {  
+    //outer loop  
+      for (int j=0; j<=i; j++)  
+      {  
+      //prints star and space  
+      System.out.print("*" + " ");  
+      }  
+    //throws the cursor in the next line after printing each line  
+    System.out.println();  
+    }  
+    }  
+} 
     
-  }
-}
+                      
+
